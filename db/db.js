3e0938cf45a12c6ae7d1bcf721996fa9fcdf89ad
Raul <raulscz10@gmail.com>
2023-04-17 13:21:54 +0200

    Update

--- db/db.js
+++ db/db.js
@@ -1,4 +1,3 @@
-const config = require("../config/config.json");
 const { Sequelize } = require("sequelize");
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
