df7464d41b12772163b73edcbb6c889fc0a97d5f
Raul <raulscz10@gmail.com>
2023-04-17 13:18:15 +0200

    Fix Bug

--- db/db.js
+++ db/db.js
@@ -1,26 +1,39 @@
 const config = require("../config/config.json");
 const { Sequelize } = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
 
-const sequelize = new Sequelize(
-   process.env.MYSQL_DATABASE || config.development.database,
-   process.env.MYSQL_USER || config.development.username,
-   process.env.MYSQL_PASSWORD || config.development.password,
-   {
-      host: process.env.MYSQL_HOST || config.development.host,
-      port: process.env.MYSQL_PORT || config.development.port || 3306,
-      dialect: "mysql",
-      pool: {
-         max: 5,
-         min: 0,
-         acquire: 30000,
-         idle: 10000,
-      },
-   }
-);
+// const sequelize = new Sequelize(
+//    process.env.MYSQL_DATABASE || config.development.database,
+//    process.env.MYSQL_USER || config.development.username,
+//    process.env.MYSQL_PASSWORD || config.development.password,
+//    {
+//       host: process.env.MYSQL_HOST || config.development.host,
+//       port: process.env.MYSQL_PORT || config.development.port || 3306,
+//       dialect: "mysql",
+//       pool: {
+//          max: 5,
+//          min: 0,
+//          acquire: 30000,
+//          idle: 10000,
+//       },
+//    }
+// );
+
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 module.exports = async () => {
-   const dbConnection = await sequelize.authenticate();
-   const database = process.env.MYSQL_DATABASE || config.development.database;
-   console.log(`Successfully connected to '${database}' database`);
-   return dbConnection;
+  const dbConnection = await sequelize.authenticate();
+  const database = process.env.MYSQL_DATABASE || config.development.database;
+  console.log(`Successfully connected to '${database}' database`);
+  return dbConnection;
 };
